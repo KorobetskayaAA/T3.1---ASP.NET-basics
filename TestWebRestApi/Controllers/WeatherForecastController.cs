39ef18650948bedf031c6c03d682ba0ff351f272
A. Korobetskaya <kornast@yandex.ru>
2021-12-05 19:04:21 +0400

    Add project files.

---
+++ TestWebRestApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebRestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        private static List<WeatherForecast> weatherForecasts;
+
+        static WeatherForecastController()
+        {
+            var rng = new Random();
+            weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToList();
+        }
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return weatherForecasts;
+        }
+
+        [HttpPost] 
+        public void Post([FromBody] WeatherForecast weatherForecast)
+        {
+            weatherForecasts.Add(weatherForecast);
+        }
+    }
+}
